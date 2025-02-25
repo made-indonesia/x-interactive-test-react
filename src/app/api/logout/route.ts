2fab1444b1f5f8bdf52db8bb61a5033d2e466869
akbar <akbarabay713@gmail.com>
2025-02-25 16:37:36 +0700

    feat: remove log

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -1,12 +1,8 @@
 import {NextResponse} from "next/server";
 
 export function GET(request: Request) {
-  console.log("tes");
-
-  // ✅ Use absolute URL for redirection
   const response = NextResponse.redirect(new URL("/login", request.url));
 
-  // 🧹 Delete the accessToken cookie
   response.cookies.delete("accessToken");
   response.cookies.delete("jwtToken");
   response.cookies.delete("refreshToken");
